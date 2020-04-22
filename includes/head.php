75bcd5a566fd2b7d9593f3c77af4fb7de7b8c7ec
Luuk <99051050@mydavinci.nl>
2020-04-22 11:33:03 +0200

    Update to the response lay-out

--- includes/head.php
+++ includes/head.php
@@ -12,7 +12,7 @@ header('Content-type: text/html; charset=iso-8859-1');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <!-- <script src="https://kit.fontawesome.com/6e218605e4.js" crossorigin="anonymous"></script> -->
+    <script src="https://kit.fontawesome.com/6e218605e4.js" crossorigin="anonymous"></script>
     <title>ToDoList</title>
     <script src="js/script.js"></script>
 </head>
